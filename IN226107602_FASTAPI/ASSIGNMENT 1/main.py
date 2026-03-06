48e34ad45b0c8753b46f6fbeec6217b34f86cc8f
Suvethaa1512 <suvethaa1512@gmail.com>
2026-03-06 23:45:23 +0530

    Create main.py

---
+++ IN226107602_FASTAPI/ASSIGNMENT 1/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to FastAPI Assignment"}
+
+@app.get("/add")
+def add(a: int, b: int):
+    return {"result": a + b}
+
+@app.get("/subtract")
+def subtract(a: int, b: int):
+    return {"result": a - b}
+
+@app.get("/multiply")
+def multiply(a: int, b: int):
+    return {"result": a * b}
+
+@app.get("/divide")
+def divide(a: int, b: int):
+    return {"result": a / b}
